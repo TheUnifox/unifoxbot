eff470503c860fbb24d4a456b2e63f6cc6d68b94
Goldngamr5 <awsomejojop@gmail.com>
2022-02-10 12:14:28 -0700

    Update furryCommands.py

--- cogs/furryCommands.py
+++ cogs/furryCommands.py
@@ -59,7 +59,7 @@ class NSFWFurryCommands(commands.Cog, name="NSFW Furry Commands", description="T
 		cs = aiohttp.ClientSession()
 		print('got client session')
 		headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
-		r = await cs.get("https://e621.net/posts.json?tags=gay", headers=headers)
+		r = await cs.get(f"https://e621.net/posts.json?tags={keywords}+order:score", headers=headers)
 		print('got e6 link')
 		print(r)
 		data = await r.json(content_type=None)
