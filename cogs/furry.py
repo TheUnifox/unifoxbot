5ef92f94b189749f17f45297d05bb7591662efdc
Goldngamr5 <awsomejojop@gmail.com>
2022-02-14 08:49:44 -0700

    Added tags to not show in results
    made some stuff subtracted from the results in yiff command

--- cogs/furry.py
+++ cogs/furry.py
@@ -84,7 +84,7 @@ class NSFWFurryCommands(commands.Cog, name="NSFW Furry Commands", description="T
 			cs = aiohttp.ClientSession()
 			print('got client session')
 			headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
-			r = await cs.get(f'https://e621.net/posts.json?tags={keywords}+order:score+type:jpg+type:png&limit=50', headers=headers)
+			r = await cs.get(f'https://e621.net/posts.json?tags={keywords}+-watersports+-scat+-vore+-gore+-loli+-shota+order:score+type:jpg+type:png&limit=50', headers=headers)
 			print('got e6 link')
 			print(r.status)
 			if r.status == 200:
