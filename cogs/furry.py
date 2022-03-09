3855ce662da041aba3da17ad1fdaa534b18d33b0
Goldngamr5 <awsomejojop@gmail.com>
2022-03-09 08:37:47 -0700

    nsfw furry commands work in dms now, uses check func

--- cogs/furry.py
+++ cogs/furry.py
@@ -214,7 +214,7 @@ class NSFWFurryCommands(commands.Cog, name="NSFW Furry Commands", description="T
 
 	@commands.command(name='post', help='tries to get the post with the given id')
 	async def post(self, ctx, postid: int):
-		if ctx.channel.is_nsfw() or isinstance(ctx.channel, discord.channel.DMChannel):
+		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
 			cs = aiohttp.ClientSession()
 			print('got client session')
 			headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
@@ -246,7 +246,7 @@ class NSFWFurryCommands(commands.Cog, name="NSFW Furry Commands", description="T
 
 	@commands.command(name='e6anim', help='searches e621 for videos based off a search term')
 	async def e6anim(self, ctx, *, search='gay'):
-		if ctx.channel.is_nsfw() or isinstance(ctx.channel, discord.channel.DMChannel):
+		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
 			tosearch=search
 			keywords, searchwords = GoogleSearch.key_words_search_words(GoogleSearch, user_message=tosearch)
 			print(f'got keywords: {keywords}\n from {search}')
@@ -274,7 +274,7 @@ class NSFWFurryCommands(commands.Cog, name="NSFW Furry Commands", description="T
 
 	@commands.command(name='randyiff', help='Searches e621.net based off a search term, sorts randomly')
 	async def randyiff(self, ctx, *, search='gay'):
-		if ctx.channel.is_nsfw() or isinstance(ctx.channel, discord.channel.DMChannel):
+		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
 			tosearch=search
 			keywords, searchwords = GoogleSearch.key_words_search_words(GoogleSearch, user_message=tosearch)
 			print(f'got keywords: {keywords}\n from {search}')
@@ -306,7 +306,7 @@ class NSFWFurryCommands(commands.Cog, name="NSFW Furry Commands", description="T
 
 	@commands.command(name='rande6anim', help='searches e621 for videos based off a search term, sorts randomly')
 	async def rande6anim(self, ctx, *, search='gay'):
-		if ctx.channel.is_nsfw() or isinstance(ctx.channel, discord.channel.DMChannel):
+		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
 			tosearch=search
 			keywords, searchwords = GoogleSearch.key_words_search_words(GoogleSearch, user_message=tosearch)
 			print(f'got keywords: {keywords}\n from {search}')
@@ -334,7 +334,7 @@ class NSFWFurryCommands(commands.Cog, name="NSFW Furry Commands", description="T
 
 	@commands.command(name='uwu', help='uwu copypasta lol')
 	async def uwu(self, ctx):
-		if ctx.channel.is_nsfw() or isinstance(ctx.channel, discord.channel.DMChannel):
+		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
 			await ctx.send("Rawr x3 nuzzles how are you pounces on you you're so warm o3o notices you have a bulge o: someone's happy (; nuzzles your necky wecky~ murr~ hehehe rubbies your bulgy wolgy you're so big :oooo rubbies more on your bulgy wolgy it doesn't stop growing ·///· kisses you and lickies your necky daddy likies (; nuzzles wuzzles I hope daddy really likes $: wiggles butt and squirms I want to see your big daddy meat~ wiggles butt I have a little itch o3o wags tail can you please get my itch~ puts paws on your chest nyea~ its a seven inch itch rubs your chest can you help me pwease squirms pwetty pwease sad face I need to be punished runs paws down your chest and bites lip like I need to be punished really good~ paws on your bulge as I lick my lips I'm getting thirsty. I can go for some milk unbuttons your pants as my eyes glow you smell so musky :v licks shaft mmmm~ so musky drools all over your cock your daddy meat I like fondles Mr. Fuzzy Balls hehe puts snout on balls and inhales deeply oh god im so hard~ licks balls punish me daddy~ nyea~ squirms more and wiggles butt I love your musky goodness bites lip please punish me licks lips nyea~ suckles on your tip so good licks pre of your cock salty goodness~ eyes role back and goes balls deep mmmm~ moans and suckles")
 		else:
 			await ctx.send('Command must be used in nsfw channel!!!')
