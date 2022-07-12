8cd5c185bf80b18bec11135d994f9b4b43f094ff
Goldngamr5 <70986642+Goldngamr5@users.noreply.github.com>
2022-07-12 10:56:02 -0700

    get random colour using user as seed
    there was no yellow :(

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ class Main():
 					print(f"serching channel {channel.name}")
 					if channel.name == 'welcomes':
 						print("found channel")
-						embed=discord.Embed(title="Welcome!", color=discord.Colour.yellow())
+						embed=discord.Embed(title="Welcome!", color=discord.Colour.random(seed=(bytes(member))))
 						embed.set_thumbnail(url=member.avatar_url)
 						embed.add_field(name=f"Welcome to {server.name}", value=f"You are the {(len(server.members)-15)} attendee here! Head to #rules to read and accept, and become a part of this convention! The next convention is Oct. 14-16! We hope you have fun here!")
 						await channel.send(embed=embed)
