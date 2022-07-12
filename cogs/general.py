7578806439fcf29a0db606c080d68bdfe4fff597
Goldngamr5 <70986642+Goldngamr5@users.noreply.github.com>
2022-07-11 17:11:38 -0700

    send avatar url in value ig

--- cogs/general.py
+++ cogs/general.py
@@ -63,7 +63,7 @@ class GeneralCommands(commands.Cog, name='General Commands', description='Comman
 			user = ctx.author
 		embed = discord.Embed(title=f"{user} Member Info", color=user.color)
 		embed.set_thumbnail(url=user.avatar_url)
-		embed.add_field(name="User avatar: ", value=user.avatar, inline=True)
+		embed.add_field(name="User avatar: ", value=user.avatar_url, inline=True)
 		if int(user.id) == Main.bot.author_id:
 			await ctx.send("Why hello my creator!")
 		await ctx.send(embed=embed)
