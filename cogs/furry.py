bc8386626a2e8ed62db1c16208900f7d539ffad3
Unifox <70986642+TheUnifox@users.noreply.github.com>
2022-09-29 10:34:09 -0700

    Fix commands nsfw check

--- cogs/furry.py
+++ cogs/furry.py
@@ -182,7 +182,7 @@ class NsfwFurryCommands(commands.Cog, name="Nsfw Furry Commands", description="T
 	#makes sure the post isn't a webm, because I couldn't get it working with webm video
 	@commands.command(name='yiff', help='Searches e621.net based off a search term')
 	async def yiff(self, ctx, *, search='gay'):
-		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
+		if await NsfwFurryCommands.nsfwcheck(ctx.channel):
 			tosearch=search
 			keywords, searchwords = GoogleSearch.key_words_search_words(GoogleSearch, user_message=tosearch)
 			print(f'got keywords: {keywords}\n from {search}')
@@ -214,7 +214,7 @@ class NsfwFurryCommands(commands.Cog, name="Nsfw Furry Commands", description="T
 
 	@commands.command(name='post', help='tries to get the post with the given id')
 	async def post(self, ctx, postid: int):
-		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
+		if await NsfwFurryCommands.nsfwcheck(ctx.channel):
 			cs = aiohttp.ClientSession()
 			print('got client session')
 			headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
@@ -246,7 +246,7 @@ class NsfwFurryCommands(commands.Cog, name="Nsfw Furry Commands", description="T
 			
 	@commands.command(name='animpost', help='tries to get the post with the given id. for animations bc post only works for images/gif')
 	async def animpost(self, ctx, postid: int):
-		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
+		if await NsfwFurryCommands.nsfwcheck(ctx.channel):
 			cs = aiohttp.ClientSession()
 			print('got client session')
 			headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
@@ -272,7 +272,7 @@ class NsfwFurryCommands(commands.Cog, name="Nsfw Furry Commands", description="T
 
 	@commands.command(name='e6anim', help='searches e621 for videos based off a search term')
 	async def e6anim(self, ctx, *, search='gay'):
-		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
+		if await NsfwFurryCommands.nsfwcheck(ctx.channel):
 			tosearch=search
 			keywords, searchwords = GoogleSearch.key_words_search_words(GoogleSearch, user_message=tosearch)
 			print(f'got keywords: {keywords}\n from {search}')
@@ -300,7 +300,7 @@ class NsfwFurryCommands(commands.Cog, name="Nsfw Furry Commands", description="T
 
 	@commands.command(name='randyiff', help='Searches e621.net based off a search term, sorts randomly')
 	async def randyiff(self, ctx, *, search='gay'):
-		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
+		if await NsfwFurryCommands.nsfwcheck(ctx.channel):
 			tosearch=search
 			keywords, searchwords = GoogleSearch.key_words_search_words(GoogleSearch, user_message=tosearch)
 			print(f'got keywords: {keywords}\n from {search}')
@@ -332,7 +332,7 @@ class NsfwFurryCommands(commands.Cog, name="Nsfw Furry Commands", description="T
 
 	@commands.command(name='rande6anim', help='searches e621 for videos based off a search term, sorts randomly')
 	async def rande6anim(self, ctx, *, search='gay'):
-		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
+		if await NsfwFurryCommands.nsfwcheck(ctx.channel):
 			tosearch=search
 			keywords, searchwords = GoogleSearch.key_words_search_words(GoogleSearch, user_message=tosearch)
 			print(f'got keywords: {keywords}\n from {search}')
@@ -360,7 +360,7 @@ class NsfwFurryCommands(commands.Cog, name="Nsfw Furry Commands", description="T
 
 	@commands.command(name='uwu', help='uwu copypasta lol')
 	async def uwu(self, ctx):
-		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
+		if await NsfwFurryCommands.nsfwcheck(ctx.channel):
 			await ctx.send("Rawr x3 nuzzles how are you pounces on you you're so warm o3o notices you have a bulge o: someone's happy (; nuzzles your necky wecky~ murr~ hehehe rubbies your bulgy wolgy you're so big :oooo rubbies more on your bulgy wolgy it doesn't stop growing ·///· kisses you and lickies your necky daddy likies (; nuzzles wuzzles I hope daddy really likes $: wiggles butt and squirms I want to see your big daddy meat~ wiggles butt I have a little itch o3o wags tail can you please get my itch~ puts paws on your chest nyea~ its a seven inch itch rubs your chest can you help me pwease squirms pwetty pwease sad face I need to be punished runs paws down your chest and bites lip like I need to be punished really good~ paws on your bulge as I lick my lips I'm getting thirsty. I can go for some milk unbuttons your pants as my eyes glow you smell so musky :v licks shaft mmmm~ so musky drools all over your cock your daddy meat I like fondles Mr. Fuzzy Balls hehe puts snout on balls and inhales deeply oh god im so hard~ licks balls punish me daddy~ nyea~ squirms more and wiggles butt I love your musky goodness bites lip please punish me licks lips nyea~ suckles on your tip so good licks pre of your cock salty goodness~ eyes role back and goes balls deep mmmm~ moans and suckles")
 		else:
 			await ctx.send('Command must be used in nsfw channel!!!')
@@ -384,7 +384,7 @@ class NsfwFurryCommandsplus(commands.Cog, name="Nsfw Furry Commands+", descripti
 	#makes sure the post isn't a webm, because I couldn't get it working with webm video
 	@commands.command(name='yiff+', help='Searches e621.net based off a search term extra')
 	async def yiffplus(self, ctx, *, search='gay'):
-		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
+		if await NsfwFurryCommands.nsfwcheck(ctx.channel):
 			tosearch=search
 			keywords, searchwords = GoogleSearch.key_words_search_words(GoogleSearch, user_message=tosearch)
 			print(f'got keywords: {keywords}\n from {search}')
@@ -416,7 +416,7 @@ class NsfwFurryCommandsplus(commands.Cog, name="Nsfw Furry Commands+", descripti
 
 	@commands.command(name='e6anim+', help='searches e621 for videos based off a search term')
 	async def e6animplus(self, ctx, *, search='gay'):
-		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
+		if await NsfwFurryCommands.nsfwcheck(ctx.channel):
 			tosearch=search
 			keywords, searchwords = GoogleSearch.key_words_search_words(GoogleSearch, user_message=tosearch)
 			print(f'got keywords: {keywords}\n from {search}')
@@ -444,7 +444,7 @@ class NsfwFurryCommandsplus(commands.Cog, name="Nsfw Furry Commands+", descripti
 
 	@commands.command(name='randyiff+', help='Searches e621.net based off a search term, sorts randomly')
 	async def randyiffplus(self, ctx, *, search='gay'):
-		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
+		if await NsfwFurryCommands.nsfwcheck(ctx.channel):
 			tosearch=search
 			keywords, searchwords = GoogleSearch.key_words_search_words(GoogleSearch, user_message=tosearch)
 			print(f'got keywords: {keywords}\n from {search}')
@@ -476,7 +476,7 @@ class NsfwFurryCommandsplus(commands.Cog, name="Nsfw Furry Commands+", descripti
 
 	@commands.command(name='rande6anim+', help='searches e621 for videos based off a search term, sorts randomly')
 	async def rande6anim(self, ctx, *, search='gay'):
-		if await NSFWFurryCommands.nsfwcheck(ctx.channel):
+		if await NsfwFurryCommands.nsfwcheck(ctx.channel):
 			tosearch=search
 			keywords, searchwords = GoogleSearch.key_words_search_words(GoogleSearch, user_message=tosearch)
 			print(f'got keywords: {keywords}\n from {search}')
